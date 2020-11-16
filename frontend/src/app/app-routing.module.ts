843ff39304ac75da4614eeb612817d7fd2108817
Végvári Vilmos <vegvari.vili@gmail.com>
2020-11-16 22:51:24 +0100

    Added auth guard for routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,11 +4,17 @@ import { AuthComponent } from './auth/auth.component';
 import { ReminderComponent } from './reminder/reminder.component';
 import { SettingsComponent } from './settings/settings.component';
 
+import { AuthGuard } from './auth/auth-guard';
+
 const routes: Routes = [
   { path: '', redirectTo: 'reminders', pathMatch: 'full' },
   { path: 'auth', component: AuthComponent },
-  { path: 'reminders', component: ReminderComponent },
-  { path: 'settings', component: SettingsComponent },
+  {
+    path: 'reminders',
+    component: ReminderComponent,
+    canActivate: [AuthGuard],
+  },
+  { path: 'settings', component: SettingsComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
