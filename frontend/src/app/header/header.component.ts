7d7d6b68fffa979b45a74cfbf0f2d531ca9ef739
Végvári Vilmos <vegvari.vili@gmail.com>
2020-11-16 23:32:02 +0100

    header fixes

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  isAuthenticated = false;
+  private userSub: Subscription;
 
-  constructor() { }
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
+    this.userSub = this.authService.user.subscribe((user) => {
+      this.isAuthenticated = !!user;
+    });
+  }
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe();
   }
 
+  onLogout() {
+    if (!this.isAuthenticated) {
+      return;
+    }
+    this.authService.logout();
+  }
 }
