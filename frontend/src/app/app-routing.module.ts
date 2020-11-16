d62bc3ee6870cc2ddcd3807c37f874a2662c5572
Végvári Vilmos <vegvari.vili@gmail.com>
2020-11-16 21:47:24 +0100

    Routing added

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { ReminderComponent } from './reminder/reminder.component';
+import { SettingsComponent } from './settings/settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'reminders', pathMatch: 'full' },
+  { path: 'auth', component: AuthComponent },
+  { path: 'reminders', component: ReminderComponent },
+  { path: 'settings', component: SettingsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
