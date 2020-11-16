e234d566ce417a5cfa83fae4e2c5fd62cb6c5cec
Végvári Vilmos <vegvari.vili@gmail.com>
2020-11-16 21:37:12 +0100

    basic component structure

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { HeaderComponent } from './header/header.component';
+import { ReminderComponent } from './reminder/reminder.component';
+import { ReminderListComponent } from './reminder/reminder-list/reminder-list.component';
+import { ReminderAddComponent } from './reminder/reminder-add/reminder-add.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    HeaderComponent,
+    ReminderComponent,
+    ReminderListComponent,
+    ReminderAddComponent,
+    SettingsComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ frontend/src/app/reminder/reminder-add/reminder-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reminder-add',
+  templateUrl: './reminder-add.component.html',
+  styleUrls: ['./reminder-add.component.css']
+})
+export class ReminderAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
